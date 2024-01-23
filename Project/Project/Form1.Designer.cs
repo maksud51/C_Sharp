5a0fc724a2834abdc441443ed4a0d0a530da3087
Md Al Maksud Alam <91654429+maksud51@users.noreply.github.com>
2024-01-23 22:58:58 +0600

    Add files via upload

---
+++ Project/Project/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace Project
+{
+    partial class registrationUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SergentName = new System.Windows.Forms.TextBox();
+            this.sergentNameLabel = new System.Windows.Forms.Label();
+            this.sergentDob = new System.Windows.Forms.DateTimePicker();
+            this.dateOfBirthLabel = new System.Windows.Forms.Label();
+            this.sergentGender = new System.Windows.Forms.ComboBox();
+            this.postalCode = new System.Windows.Forms.TextBox();
+            this.passoword = new System.Windows.Forms.TextBox();
+            this.genderLabel = new System.Windows.Forms.Label();
+            this.postalCodeLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sergentUserName = new System.Windows.Forms.TextBox();
+            this.UserNameLabel = new System.Windows.Forms.Label();
+            this.sergentPicture = new System.Windows.Forms.PictureBox();
+            this.sergentPictureButton = new System.Windows.Forms.Button();
+            this.sergentSignUpButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sergentPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SergentName
+            // 
+            this.SergentName.ForeColor = System.Drawing.Color.Gray;
+            this.SergentName.Location = new System.Drawing.Point(286, 69);
+            this.SergentName.Name = "SergentName";
+            this.SergentName.Size = new System.Drawing.Size(317, 22);
+            this.SergentName.TabIndex = 0;
+            this.SergentName.Text = "Sergent Name";
+            // 
+            // sergentNameLabel
+            // 
+            this.sergentNameLabel.AutoSize = true;
+            this.sergentNameLabel.Location = new System.Drawing.Point(202, 72);
+            this.sergentNameLabel.Name = "sergentNameLabel";
+            this.sergentNameLabel.Size = new System.Drawing.Size(49, 17);
+            this.sergentNameLabel.TabIndex = 11;
+            this.sergentNameLabel.Text = "Name:";
+            // 
+            // sergentDob
+            // 
+            this.sergentDob.CalendarForeColor = System.Drawing.Color.Gray;
+            this.sergentDob.CalendarTitleForeColor = System.Drawing.Color.Black;
+            this.sergentDob.Location = new System.Drawing.Point(286, 207);
+            this.sergentDob.Name = "sergentDob";
+            this.sergentDob.Size = new System.Drawing.Size(317, 22);
+            this.sergentDob.TabIndex = 2;
+            // 
+            // dateOfBirthLabel
+            // 
+            this.dateOfBirthLabel.AutoSize = true;
+            this.dateOfBirthLabel.Location = new System.Drawing.Point(157, 207);
+            this.dateOfBirthLabel.Name = "dateOfBirthLabel";
+            this.dateOfBirthLabel.Size = new System.Drawing.Size(94, 17);
+            this.dateOfBirthLabel.TabIndex = 11;
+            this.dateOfBirthLabel.Text = "Date Of Birth:";
+            // 
+            // sergentGender
+            // 
+            this.sergentGender.ForeColor = System.Drawing.Color.Gray;
+            this.sergentGender.FormattingEnabled = true;
+            this.sergentGender.Items.AddRange(new object[] {
+            "Male",
+            "Female",
+            "Others"});
+            this.sergentGender.Location = new System.Drawing.Point(286, 273);
+            this.sergentGender.Name = "sergentGender";
+            this.sergentGender.Size = new System.Drawing.Size(317, 24);
+            this.sergentGender.TabIndex = 3;
+            this.sergentGender.Text = "Gender";
+            // 
+            // postalCode
+            // 
+            this.postalCode.ForeColor = System.Drawing.Color.Gray;
+            this.postalCode.Location = new System.Drawing.Point(286, 337);
+            this.postalCode.Name = "postalCode";
+            this.postalCode.Size = new System.Drawing.Size(317, 22);
+            this.postalCode.TabIndex = 4;
+            this.postalCode.Tag = "";
+            this.postalCode.Text = "Postal Code";
+            // 
+            // passoword
+            // 
+            this.passoword.ForeColor = System.Drawing.Color.Gray;
+            this.passoword.Location = new System.Drawing.Point(286, 395);
+            this.passoword.Name = "passoword";
+            this.passoword.Size = new System.Drawing.Size(317, 22);
+            this.passoword.TabIndex = 5;
+            this.passoword.Text = "Password";
+            // 
+            // genderLabel
+            // 
+            this.genderLabel.AutoSize = true;
+            this.genderLabel.Location = new System.Drawing.Point(191, 273);
+            this.genderLabel.Name = "genderLabel";
+            this.genderLabel.Size = new System.Drawing.Size(60, 17);
+            this.genderLabel.TabIndex = 11;
+            this.genderLabel.Text = "Gender:";
+            // 
+            // postalCodeLabel
+            // 
+            this.postalCodeLabel.AutoSize = true;
+            this.postalCodeLabel.Location = new System.Drawing.Point(202, 337);
+            this.postalCodeLabel.Name = "postalCodeLabel";
+            this.postalCodeLabel.Size = new System.Drawing.Size(35, 17);
+            this.postalCodeLabel.TabIndex = 11;
+            this.postalCodeLabel.Text = "PIO:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(178, 398);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Password:";
+            // 
+            // sergentUserName
+            // 
+            this.sergentUserName.ForeColor = System.Drawing.Color.Gray;
+            this.sergentUserName.Location = new System.Drawing.Point(286, 139);
+            this.sergentUserName.Name = "sergentUserName";
+            this.sergentUserName.Size = new System.Drawing.Size(317, 22);
+            this.sergentUserName.TabIndex = 1;
+            this.sergentUserName.Text = "User Name";
+            // 
+            // UserNameLabel
+            // 
+            this.UserNameLabel.AutoSize = true;
+            this.UserNameLabel.Location = new System.Drawing.Point(157, 142);
+            this.UserNameLabel.Name = "UserNameLabel";
+            this.UserNameLabel.Size = new System.Drawing.Size(83, 17);
+            this.UserNameLabel.TabIndex = 11;
+            this.UserNameLabel.Text = "User Name:";
+            // 
+            // sergentPicture
+            // 
+            this.sergentPicture.Location = new System.Drawing.Point(712, 93);
+            this.sergentPicture.Name = "sergentPicture";
+            this.sergentPicture.Size = new System.Drawing.Size(182, 204);
+            this.sergentPicture.TabIndex = 14;
+            this.sergentPicture.TabStop = false;
+            // 
+            // sergentPictureButton
+            // 
+            this.sergentPictureButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sergentPictureButton.Location = new System.Drawing.Point(748, 321);
+            this.sergentPictureButton.Name = "sergentPictureButton";
+            this.sergentPictureButton.Size = new System.Drawing.Size(125, 38);
+            this.sergentPictureButton.TabIndex = 6;
+            this.sergentPictureButton.Text = "Upload";
+            this.sergentPictureButton.UseVisualStyleBackColor = true;
+            // 
+            // sergentSignUpButton
+            // 
+            this.sergentSignUpButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sergentSignUpButton.Location = new System.Drawing.Point(275, 460);
+            this.sergentSignUpButton.Name = "sergentSignUpButton";
+            this.sergentSignUpButton.Size = new System.Drawing.Size(328, 38);
+            this.sergentSignUpButton.TabIndex = 7;
+            this.sergentSignUpButton.Text = "Sign Up";
+            this.sergentSignUpButton.UseVisualStyleBackColor = true;
+            this.sergentSignUpButton.Click += new System.EventHandler(this.sergentSignUpButton_Click);
+            this.sergentSignUpButton.Enter += new System.EventHandler(this.sergentSignUpButton_Click);
+            // 
+            // registrationUI
+            // 
+            this.AcceptButton = this.sergentSignUpButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(963, 553);
+            this.Controls.Add(this.sergentSignUpButton);
+            this.Controls.Add(this.sergentPictureButton);
+            this.Controls.Add(this.sergentPicture);
+            this.Controls.Add(this.UserNameLabel);
+            this.Controls.Add(this.sergentUserName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.postalCodeLabel);
+            this.Controls.Add(this.genderLabel);
+            this.Controls.Add(this.passoword);
+            this.Controls.Add(this.postalCode);
+            this.Controls.Add(this.sergentGender);
+            this.Controls.Add(this.dateOfBirthLabel);
+            this.Controls.Add(this.sergentDob);
+            this.Controls.Add(this.sergentNameLabel);
+            this.Controls.Add(this.SergentName);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "registrationUI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registration";
+            ((System.ComponentModel.ISupportInitialize)(this.sergentPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SergentName;
+        private System.Windows.Forms.Label sergentNameLabel;
+        private System.Windows.Forms.DateTimePicker sergentDob;
+        private System.Windows.Forms.Label dateOfBirthLabel;
+        private System.Windows.Forms.ComboBox sergentGender;
+        private System.Windows.Forms.TextBox postalCode;
+        private System.Windows.Forms.TextBox passoword;
+        private System.Windows.Forms.Label genderLabel;
+        private System.Windows.Forms.Label postalCodeLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox sergentUserName;
+        private System.Windows.Forms.Label UserNameLabel;
+        private System.Windows.Forms.PictureBox sergentPicture;
+        private System.Windows.Forms.Button sergentPictureButton;
+        private System.Windows.Forms.Button sergentSignUpButton;
+    }
+}
+
---
+++ Project/Project/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class registrationUI : Form
+    {
+        public registrationUI()
+        {
+            InitializeComponent();
+        }
+
+        private void sergentSignUpButton_Click(object sender, EventArgs e)
+        {
+            //Form Validation
+
+            if (SergentName.Text.Trim()=="")
+            {
+                MessageBox.Show("Empty Name box.\n Please Enter Your Name.");
+                SergentName.Focus();
+                return;
+            }
+
+            if (SergentName.Text.Length > 11)
+            {
+                MessageBox.Show("Name is too much long.Please Enter Shorter name.Maximum Length 11 Latter");
+                return;
+            }
+
+
+            if (sergentUserName.Text.Trim() == "")
+            {
+                MessageBox.Show("Empty Name box.\n Please Enter Your Name.");
+                sergentUserName.Focus();
+                return;
+            }
+
+            if (sergentUserName.Text.Length > 10 )
+            {
+                MessageBox.Show("User Name is too much long. \n Please Enter Shorter User name.Maximum Length 10 Latter");
+                return;
+
+            }
+
+            if (sergentDob.Text.Trim()=="")
+            {
+                MessageBox.Show("Empty Name box.\n Please Enter Your Date Of Birth.");
+                sergentDob.Focus();
+                return;
+            }
+
+            if (sergentGender.Text.Trim()=="")
+            {
+                MessageBox.Show("Empty Name box.\n Please Enter Your Gender.");
+                sergentGender.Focus();
+                return;
+            }
+
+            if (postalCode.Text.Trim()=="")
+            {
+                MessageBox.Show("Empty Name box.\n Please Enter Your Postal Code.");
+                postalCode.Focus();
+                return;
+            }
+
+            if (passoword.Text.Trim()=="")
+            {
+                MessageBox.Show("Empty Name box.\n Please Enter Password.");
+                passoword.Focus();
+                return;
+            }
+
+
+
+        }
+    }
+}
