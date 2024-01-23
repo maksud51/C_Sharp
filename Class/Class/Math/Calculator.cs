6a45f85d7aaa2ef735bb6eba263b87f376fe7a2f
Md Al Maksud Alam <91654429+maksud51@users.noreply.github.com>
2024-01-23 22:05:00 +0600

    Add files via upload

---
+++ Class/Class/Math/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class.Math
+{
+    public class Calculator
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ Class/Class/Program.cs
@@ -0,0 +1,36 @@
+﻿using Class.Math;
+using System;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My Name is " + firstName + " " + lastName);
+            Console.ReadKey();
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person male = new Person();
+            male.firstName = "Maksud";
+            male.lastName = "Alam";
+
+            male.Introduce();
+            int result = Calculator.Add(13, 17);
+            Console.WriteLine("Add"+result);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
