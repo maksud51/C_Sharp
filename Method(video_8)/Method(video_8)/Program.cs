35dd01c482b8e842b8884a29d7a74edcf28ddb38
Md Al Maksud Alam <91654429+maksud51@users.noreply.github.com>
2024-01-23 22:40:12 +0600

    Add files via upload

---
+++ Method(video_8)/Method(video_8)/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Method_video_8_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Add1(4, 5);
+            Console.WriteLine(result);
+        }
+
+        static int Add1(int firstNumber, int secondNumber)
+        {
+            int sum = firstNumber + secondNumber;
+            return sum;
+        }
+    }
+}
