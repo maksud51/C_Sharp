5df116dcecb4b8d99c70e83c4f7e10f964b70717
Md Al Maksud Alam <91654429+maksud51@users.noreply.github.com>
2024-01-23 22:09:23 +0600

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Class/Contact.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2.Class
+{
+
+
+    internal class Contact
+{
+    private string personName;
+    private string personId;
+    private int age;
+    private string mobileNumber;
+    private char gender;
+
+    public Contact()
+    {
+
+    }
+    public Contact(string personName, string personId, int age, string mobileNumber, char gender)
+    {
+        this.personName = personName;
+        this.personId = personId;
+        this.age = age;
+        this.mobileNumber = mobileNumber;
+        this.gender = gender;
+    }
+
+    public void setPersonName(string personName)
+    {
+        this.personName = personName;
+    }
+
+    public string getPersonName()
+    {
+        return this.personName;
+    }
+
+    public void setPersonId(string personId)
+    {
+        this.personId = personId;
+    }
+
+    public string getPersonId()
+    {
+        return this.personId;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    public int getAge()
+    {
+        return this.age;
+    }
+
+    public void setMobileNumber(string mobileNumber)
+    {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public string getMobileNumber()
+    {
+        return this.mobileNumber;
+    }
+
+    public void setGender(char gender)
+    {
+        this.gender = gender;
+    }
+
+    public char getGender()
+    {
+        return this.gender;
+    }
+
+    public void showPersonInfo()
+    {
+        Console.WriteLine("Person name: " + this.personName);
+        Console.WriteLine("Person id: " + this.personId);
+        Console.WriteLine("Person age:" + this.age);
+        Console.WriteLine("Person mobile number: " + this.mobileNumber);
+        Console.WriteLine("Person gender: " + this.gender);
+    }
+
+    public void detectMobileOperator()
+    {
+        if (this.mobileNumber.StartsWith("017"))
+        {
+            Console.WriteLine("Gp");
+        }
+        else if (this.mobileNumber.StartsWith("018"))
+        {
+            Console.WriteLine("Robi");
+        }
+        else if (this.mobileNumber.StartsWith("019"))
+        {
+            Console.WriteLine("Banglalink");
+        }
+    }
+}
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class.Book b1 = new Class.Book();
+            b1.setBookName("Lions");
+            b1.setBookAuthor("Jany");
+            b1.setBookCopy(20);
+            b1.setBookId("154sd");
+            b1.setBookType("Biography");
+            b1.showInfo();
+            Class.Book.ShowTotalBookInfo();
+
+            Class.Contact p1 = new Class.Contact();
+            p1.setPersonName("Maksud");
+            p1.setAge(20);
+            p1.setGender('M');
+            p1.setMobileNumber("01856258552");
+            p1.setPersonId("sdd256sdf");
+            p1.showPersonInfo();
+            p1.detectMobileOperator();
+        }
+    }
+}
