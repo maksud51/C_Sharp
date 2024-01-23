518094a8194b99ad0d8c1cd86fa3edb4c9807c9c
Md Al Maksud Alam <91654429+maksud51@users.noreply.github.com>
2024-01-23 22:06:32 +0600

    Add files via upload

---
+++ Class1/Class1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Class1
+{
+    class Animal
+    {
+        public string name;
+        public int age;
+        public string voice;
+        public static int animalCount = 0;
+
+        public void printDetails()
+        {
+            Console.WriteLine("Name: " + name + " Age: " + age + " Voice: " +voice);
+           
+        }
+
+        //class constructor
+        public Animal(string _name, int _age, string _voice)
+        {
+            name = _name;
+            age = _age;
+            voice = _voice;
+            animalCount++;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal ob1 = new Animal("maksud", 1, "Tolk");
+            ob1.printDetails();
+
+            Animal ob2 = new Animal("Cat", 2, "Mew");
+            ob2.printDetails();
+
+
+            Animal ob3 = new Animal("Dog", 3, "Gew");
+            ob3.printDetails();
+
+            Console.WriteLine("number of Animal: " + Animal.animalCount);
+        }
+    }
+}
