3624894b85ac7c4e1a2eac9dc9203d7650ee21ff
Md Al Maksud Alam <91654429+maksud51@users.noreply.github.com>
2024-01-23 22:00:37 +0600

    Add files via upload

---
+++ Array/Array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = { "BMW", "Volbo", "Ford" };
+            cars[0] = "Toyta";
+            Console.WriteLine(cars[0]);
+            
+
+            int[] numbers = { 2, 3, 4, 5 };
+            Console.WriteLine(numbers[2]);
+            Console.ReadKey();
+
+        }
+    }
+}
