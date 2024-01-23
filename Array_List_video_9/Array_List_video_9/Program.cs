b50e1f6bcad5a0581c26058c6287b5a98875b829
Md Al Maksud Alam <91654429+maksud51@users.noreply.github.com>
2024-01-23 22:01:58 +0600

    Add files via upload

---
+++ Array_List_video_9/Array_List_video_9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Array_List_video_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int size = Convert.ToInt32(Console.ReadLine());*/
+            int[] number = new int[5];
+/*          number[0] = 20;
+            number[1] = 20;
+            number[2] = 20;
+            number[3] = 20;*/
+
+            for(int i = 0; i < number.Length; i++)
+            {
+                number[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            /*            for (int i = 0; i < number.Length; i++ )
+                        {
+                            Console.WriteLine(number[i]);
+                        }*/
+            
+            foreach (var data in number)
+            {
+                Console.WriteLine(data);
+            }
+
+            
+        }
+    }
+}
