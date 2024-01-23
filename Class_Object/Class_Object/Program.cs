0bac12fc881f23ce54e2ced9330d7ef94c0ecd24
Md Al Maksud Alam <91654429+maksud51@users.noreply.github.com>
2024-01-23 22:05:27 +0600

    Add files via upload

---
+++ Class_Object/Class_Object/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Class_Object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
