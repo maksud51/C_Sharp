17dff608ec7b8233f3d1b9b453eb99b1e6dc84b7
Md Al Maksud Alam <91654429+maksud51@users.noreply.github.com>
2024-01-23 23:03:11 +0600

    Add files via upload

---
+++ Project1/Project1/RegistrationForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Project1
+{
+    partial class registrationFormUi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Name = new System.Windows.Forms.TextBox();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.SignUpButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Name
+            // 
+            this.Name.ForeColor = System.Drawing.Color.Gray;
+            this.Name.Location = new System.Drawing.Point(278, 82);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(317, 22);
+            this.Name.TabIndex = 2;
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(125, 85);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(49, 17);
+            this.NameLabel.TabIndex = 12;
+            this.NameLabel.Text = "Name:";
+            // 
+            // SignUpButton
+            // 
+            this.SignUpButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SignUpButton.Location = new System.Drawing.Point(267, 137);
+            this.SignUpButton.Name = "SignUpButton";
+            this.SignUpButton.Size = new System.Drawing.Size(328, 38);
+            this.SignUpButton.TabIndex = 13;
+            this.SignUpButton.Text = "Sign Up";
+            this.SignUpButton.UseVisualStyleBackColor = true;
+            this.SignUpButton.Click += new System.EventHandler(this.SignUpButton_Click);
+            // 
+            // registrationFormUi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(941, 623);
+            this.Controls.Add(this.SignUpButton);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.Name);
+            this.Name = "registrationFormUi";
+            this.Text = "Registration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Name;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Button SignUpButton;
+    }
+}
---
+++ Properties/Properties/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Properties
+{
+    class Income
+    {
+        private int _income = 45000;
+        public int income { 
+            get { return _income; }
+            set {
+                if (value <= 0) {
+                    _income = 0;
+                }
+
+                else if (value >= 45000)
+                {
+                    _income = 45000;
+                }
+
+                else
+                {
+                    _income = value;
+                }
+            } 
+        }
+
+
+        public void Debit(int monyspend)
+        {
+            _income = _income - monyspend;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* DateTime currntTime = DateTime.Now;
+             Console.WriteLine(currntTime);
+             Console.ReadKey();*/
+            Income amountleft = new Income();
+            amountleft.Debit(25000);
+            Console.WriteLine(amountleft.income);
+            amountleft.income = amountleft.income - 50000;
+            Console.WriteLine(amountleft.income);
+            amountleft.income += 70000;
+            Console.WriteLine(amountleft.income);
+        }
+    }
+}
